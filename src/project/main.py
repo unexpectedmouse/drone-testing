6249784184363d203620f12c5dd481d12d6aecb5
unexpectedmouse <artand321123@mail.ru>
2025-08-11 16:31:01 +0300

    finding robots

--- src/project/main.py
+++ src/project/main.py
@@ -1,21 +1,20 @@
 from pion import Pion
 from threading import Thread
 from time import sleep
+from ultralytics import YOLO
 import cv2
 
 from camera import Camera
 
-
+# config
 height = 2
 ip = '10.1.100.160'
 port = 5656
 camera_ip = 'rtsp://10.1.100.160:8554/pioneer_stream'
+model_path = '../../best.pt'
+
 drone = Pion(ip,port)
-
-
-def goto(x, y: float):
-    drone.goto(x, y, height, 0)
-    sleep(2.5)
+model = YOLO(model_path)
 
 
 def photo():
@@ -23,10 +22,27 @@ def photo():
 
     while True:
         frame = camera.get()
+
+        results = model.predict(frame)
+        print('-> ', end='')
+        for result in results:
+            if result.boxes == None:
+                continue
+
+            position = result.boxes.xywh
+            frame = cv2.drawMarker(frame, (int(position[0]), int(position[1])), (255,0,0))
+            names = [result.names[cls.item()] for cls in result.boxes.cls.int()]
+            print(names)
+
         cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+
+def goto(x, y: float):
+    drone.goto(x, y, height, 0)
+    sleep(5)
 
 
 def fly():
