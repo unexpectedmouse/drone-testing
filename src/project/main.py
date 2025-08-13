b07be7171ed35bd3541f04028f07a6b189c0a245
MAXIM280-ship-it <kuryatkov.maxim@gmail.com>
2025-08-13 11:18:17 +0300

    bot positions

--- src/project/main.py
+++ src/project/main.py
@@ -19,6 +19,8 @@ model_path = 'best.pt'
 drone = Pion(ip,port)
 model = YOLO(model_path)
 
+cow_boxes = [(-2.83,1.61), (-2.88,-3.32), (3.55,-2.85)]
+
 frame = None
 
 
@@ -28,35 +30,52 @@ def detect():
             continue
 
         results = model.predict(frame)
-        print('-> ', end='')
+        # print('-> ', end='')
+        
         for result in results:
             if result.boxes == None:
                 continue
 
             position = result.boxes.xywh
+            bot_x,bot_y = get_geobot_coords(position[0],position[1])
+            print('bot x position:', bot_x, '\r\n', 'bot y position:', bot_y)
             # frame = cv2.drawMarker(frame, (int(position[0]), int(position[1])), (255,0,0))
+            
             names = [result.names[cls.item()] for cls in result.boxes.cls.int()]
             print(names)
-
 
 
 def photo():
     global frame
     camera = Camera(camera_ip)
     Thread(target=detect, daemon=True).start()
-
-
     while True:
         frame = camera.get()
-        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
         cv2.imshow('frame', frame)
 
+
+def get_geobot_coords(cam_x, cam_y):
+    global_x = 1.2*height*cam_x
+    global_y = 0.9*height*cam_y
+
+    drone_pos_x, drone_pos_y = drone.xyz()[0], drone.xyz()[1]
+
+    geobot_x = drone_pos_x+global_x
+    geobot_y = drone_pos_y+global_y
+
+    return geobot_x, geobot_y
+
+
+def calculate_trajectory_to():
+    pass
+
+
 def goto(x, y: float):
-    drone.goto(x, y, height, 0)
-    sleep(10)
+    drone.goto(x, y, height, 0, wait=True)
+    sleep(1)
 
 
 def fly():
@@ -84,4 +103,4 @@ def fly():
 
 
 Thread(target=photo, daemon=True).start()
-fly()
+# fly()
--- test.py
+++ test.py
@@ -1,25 +1,16 @@
-import numpy as np
-from lokky.pionmath import SSolver
-n_points = 1
-params = {
-    "kp": np.ones((n_points, 6)),
-    "ki": np.zeros((n_points, 6)),
-    "kd": np.ones((n_points, 6)) * 0,
-    "attraction_weight": 1.0,
-    "cohesion_weight": 1.0,
-    "alignment_weight": 1.0,
-    "repulsion_weight": 1.0,
-    "unstable_weight": 1.0,
-    "noise_weight": 1.0,
-    "current_velocity_weight": 0.0,
-    "safety_radius": 1.0,
-    "max_acceleration": 1.0,
-    "max_speed": 1.0,
-    "unstable_radius": 2,
-}
-solver = SSolver(params)
-drone1 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-drone2 = np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-target_matrix = np.array([4.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-state_matrix = np.array([drone1, drone2])
-new_velocities = solver.solve_for_all(state_matrix, target_matrix, 0.1)
+from pion import Pion
+from threading import Thread
+from time import sleep
+from ultralytics import YOLO
+import cv2
+
+
+drone = Pion('127.0.0.1', '8000')
+
+height = 0.4
+
+
+def goto(x, y: float):
+    drone.goto(x, y, height, 0, wait=True)
+    sleep(1)
+
